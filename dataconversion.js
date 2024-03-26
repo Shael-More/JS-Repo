f0f0adb34be076ff278a1ff065adae482c9b2ac3
Shailesh More <89303457+Shael-More@users.noreply.github.com>
2024-03-26 06:08:07 +0000

    data conversion

---
+++ dataconversion.js
@@ -0,0 +1,21 @@
+// Conversion and operations
+
+let score = NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// notes
+
+// "33" => 33
+// "21ad" => NaN
+// true => 1; false => 0;
+
+// 1 => true; 0 => false;
+// "" => false
+// "sd" => true
