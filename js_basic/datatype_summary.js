b483de81cbffdf0019b6e47bd0aa90354697cd79
Shailesh More <89303457+Shael-More@users.noreply.github.com>
2024-04-03 05:46:48 +0000

    data type s summary

---
+++ js_basic/datatype_summary.js
@@ -0,0 +1,38 @@
+// Primitive datatypes (Pass by Value)
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const name = "Mike"
+// console.log(name);
+
+const age = 22
+// console.log(age);
+
+const isLoggedIn = false
+// console.log(isLoggedIn)
+
+const temp = null
+// console.log(typeof temp)
+
+const email = undefined
+// console.log(typeof email)
+
+
+
+// Non Primitive (Reference)
+// Arrays, Objects, Functions
+
+// typeof Array, objects = object
+// typeof function = object function
+
+
+const aplhabets = ['a', 'b' , 'c']
+
+console.log(typeof aplhabets)
+
+
+const myFunction = function() {
+    console.log("Hello World")
+}
+
+console.log(typeof myFunction)
