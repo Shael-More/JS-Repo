9eac8605ac9a8fa77ab484ca16ec6d2085eeafb2
Shailesh More <89303457+Shael-More@users.noreply.github.com>
2024-04-11 05:36:16 +0000

    control flow

---
+++ 04_control _flow/one.js
@@ -0,0 +1,61 @@
+// control flow / logic flow
+
+// if
+
+const isUserloggedIn = true
+const temperature = 31
+
+// if (temperature < 20) {
+//     console.log("less than 40")
+// } else {
+//     console.log("More than 20")
+// }
+// console.log("executed");
+
+
+// conditional operators
+// <, >, <=. >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+const balance = 1000
+
+// if (balance > 500 ) console.log("test");  // implicit scope - same line 
+
+// +++++++++++++++++++++++  nesting
+// if (balance < 500) {
+//     console.log("less than ")
+// }  else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200")
+// }
+
+
+const UserLoggedIn = true
+const debitCard = true 
+
+if (UserLoggedIn && debitCard) {
+    console.log("Allow to buy courses");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 04_control _flow/truthy.js
@@ -0,0 +1,33 @@
+// truthy and false value
+
+
+// falsy values - false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values - "0", 'false', "space", [], {}, function() {} empty function
+
+
+const userEmail = []
+
+if(userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+const obj = {}
+
+if(Object.keys(obj).length === 0) {
+    console.log("object is empty");
+}
+
+// Nullishing Coalescing Operator (??): works on null and undefined
+
+let val1;
+val1 = null ?? 10
+
+console.log(val1);
+
+// Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log("less than 80 "): console.log("More than 80");
